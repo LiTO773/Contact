fa59844006fb87eb077d2837c3f27952e26eb240
LiTO773 <luispinto2000@gmail.com>
2017-01-19 23:07:29 +0000

    Windows can now be dragged! TODO:Maximize/Minimize

--- src/js/components/Drawer/WindowButton.js
+++ src/js/components/Drawer/WindowButton.js
@@ -5,7 +5,7 @@ export default class WindowButtons extends React.Component {
   render () {
     return (
       <center>
-        <div>
+        <div className='windowDiv'>
           <a className='windowButton' id='wb1' href='javascript:window.close()' />
           <div className='windowButton' id='wb2' />
           <div className='windowButton' id='wb3' />
